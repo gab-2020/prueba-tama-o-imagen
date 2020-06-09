9ab60c16cc2e135d1adcd558ade3d5a16ea1c87f
gab-2020 <gabriela.esteban.din@gmail.com>
2020-06-09 14:36:53 -0300

    Prueba fallida de modificar imagen

---
+++ README.md
@@ -0,0 +1,4 @@
+# prueba-tama-o-imagen
+
+No pude vincular el js con el html por alguna razón. Creo que el evento que cree en js es correcto.
+
---
+++ js/imgsize.js
@@ -0,0 +1,14 @@
+
+var imagen = document.getElementById ("imagen")
+console.log (imagen)
+
+
+
+
+
+imagen.addEventListener ("mouseover", function (){
+    imagen.style.width ="200%";
+
+});
+
+
